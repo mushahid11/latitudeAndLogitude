cb9188808e61d3a2499f66d42fcc4d9e10ff2999
ARFA TECHNOLOGY <mushahidhussain1112@gmail.com>
2021-02-05 22:59:55 +0500

    Initial commit

---
+++ app/src/main/java/com/eclairiose/comeclairiospractice/FetchAddessService.java
@@ -0,0 +1,18 @@
+package com.eclairiose.comeclairiospractice;
+
+import android.app.IntentService;
+import android.content.Intent;
+
+import androidx.annotation.Nullable;
+
+public class FetchAddessService extends IntentService {
+
+    public FetchAddessService() {
+        super("FetchAddessService");
+    }
+
+    @Override
+    protected void onHandleIntent(@Nullable Intent intent) {
+
+    }
+}
---
+++ app/src/main/java/com/eclairiose/comeclairiospractice/MainActivity.java
@@ -0,0 +1,118 @@
+package com.eclairiose.comeclairiospractice;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.os.Looper;
+import android.provider.FontsContract;
+import android.util.Log;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView latLong,address;
+    ProgressBar progressBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        latLong = findViewById(R.id.latlong);
+        progressBar = findViewById(R.id.progressbar);
+        address = findViewById(R.id.address);
+
+        findViewById(R.id.getCurrentLocation).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+                    ActivityCompat.requestPermissions(
+                            MainActivity.this,
+                            new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                            11
+                    );
+
+                } else {
+                    getCurrentLocation();
+                }
+            }
+
+
+        });
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == 11 && grantResults.length > 0) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                getCurrentLocation();
+            } else {
+                Toast.makeText(this, "Give Access", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    private void getCurrentLocation() {
+        progressBar.setVisibility(View.VISIBLE);
+        LocationRequest locationRequest = new LocationRequest();
+        locationRequest.setInterval(10000);
+        locationRequest.setFastestInterval(3000);
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        LocationServices.getFusedLocationProviderClient(MainActivity.this)
+
+                .requestLocationUpdates(locationRequest, new LocationCallback() {
+
+                    @Override
+                    public void onLocationResult(LocationResult locationResult) {
+                        super.onLocationResult(locationResult);
+                        LocationServices.getFusedLocationProviderClient(MainActivity.this)
+                                .removeLocationUpdates(this);
+                        if(locationRequest!=null && locationResult.getLocations().size()>0){
+
+                            int latestLocationIndex = locationResult.getLocations().size()-1;
+
+                            double latitude =
+                                    locationResult.getLocations().get(latestLocationIndex).getLatitude();
+
+                            double Longitude =
+                                    locationResult.getLocations().get(latestLocationIndex).getLongitude();
+                            latLong.setText(
+                                    String.format(
+                                            "LatLong: %s\nLangitude: %s",
+                                            latitude,
+                                            Longitude
+                                    )
+                            );
+
+                        }
+                        progressBar.setVisibility(View.GONE);
+                    }
+                }, Looper.getMainLooper());
+    }
+}
